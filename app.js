5858120fa9abb6778f0aa3c7f0cc7c20016dd7e3
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2025-09-08 09:57:35 -0400

    Add logic for calculator to work by adding  function, along with keeping DOM and LOGIC separate

--- app.js
+++ app.js
@@ -18,41 +18,75 @@ function operate(a, operator, b) {
   // this will grab the input for
   switch (operator) {
     case '+':
-      add(a, b);
-      break;
+      return add(a, b);
     case '-':
-      subtract(a, b);
-      break;
+      return subtract(a, b);
     case '*':
-      multiply(a, b);
-      break;
+      return multiply(a, b);
     case '/':
-      divide(a, b);
-      break;
+      return divide(a, b);
     default:
       console.log('Not available');
   }
 }
 
-let first_input;
-let operator;
-let second_input;
-
-let displayValue = '';
+let entries = [];
 
 function populateDisplay(e) {
-  let buttonText = e.target.textContent;
-  console.log(buttonText);
+  addingToEntries(e);
+  console.log(entries);
+  display.textContent = entries.join(' ');
+}
 
-  displayValue += buttonText;
-  //now we'll update the display's element with 'display'
-  display.textContent = displayValue;
+function addingToEntries(e) {
+  let buttonTextValue = e.target.textContent;
+  let lastEntry = entries[entries.length - 1];
+
+  // we check to see if it's a number or decimal, if it is, we continue
+  if (!isNaN(buttonTextValue) || buttonTextValue === '.') {
+    //We're going to check if entries is empty, if yes, we will push our first number to the entries. IF NOT, we will append to the latest element
+    if (entries.length === 0) {
+      entries.push(buttonTextValue);
+    } else if (!isNaN(lastEntry) || lastEntry.includes('.')) {
+      entries[entries.length - 1] += buttonTextValue;
+    } else {
+      //if it's anything else such as an operator, we add it
+      entries.push(buttonTextValue);
+    }
+  } else if (['+', '-', '/', '*'].includes(buttonTextValue)) {
+    console.log('OPERATOR PRESSED', buttonTextValue);
+    // we check if it's entries is empty, we do nothing because we don't want an operator to be the first in the entry
+
+    //else if,  will check to see if last entry is a number, we can add an operator if pressed.
+    if (entries.length === 0) {
+      //do nothing
+      console.log('Please choose a digit first');
+      return 'Please choose a digit first';
+    } else if (!['+', '-', '/', '*'].includes(lastEntry)) {
+      entries.push(buttonTextValue);
+    } else {
+      entries[entries.length - 1] = buttonTextValue;
+    }
+  }
 }
 
 //DOM STUFF BELOW
 const display = document.querySelector('.display');
-const keyPadsForNumbers = document.querySelectorAll('.number');
+const allKeyPad = document.querySelectorAll('.keypad');
+const equalButton = document.querySelector('.equal');
 
-keyPadsForNumbers.forEach((button) => {
+allKeyPad.forEach((button) => {
   button.addEventListener('click', populateDisplay);
 });
+
+equalButton.addEventListener('click', () => {
+  console.log('IM BEING PRESSED');
+  first_input = evaluateResult();
+});
+
+//
+function evaluateResult() {
+  let first_input;
+  let operator;
+  let second_input;
+}
